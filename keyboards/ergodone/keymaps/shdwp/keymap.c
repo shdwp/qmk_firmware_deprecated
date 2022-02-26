24fd23b4d95b466dd4f4af0bfb9fdfd2d0ffc7fd
Vasyl Horbachenko <vasyl.horbachenko@build1.io>
2022-02-26 17:15:07 +0100

    Back back

--- keyboards/ergodone/keymaps/shdwp/keymap.c
+++ keyboards/ergodone/keymaps/shdwp/keymap.c
@@ -30,10 +30,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [BASE_MAC] = LAYOUT_ergodox(
         // left hand
-        KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,               KC_5,                   KC_ESC,
-        KC_LSHIFT,      KC_Q,           KC_W,           KC_F,           KC_P,               KC_B,                   MAC_NT,
-        KC_ENT,         KC_A,           KC_R,           KC_S,           KC_T,               KC_G,
-        KC_TAB,         KC_Z,           KC_X,           KC_C,           KC_D,               KC_V,                   MAC_PT,
+        KC_NUBS,        KC_1,           KC_2,           KC_3,           KC_4,               KC_5,                   KC_ESC,
+        KC_LSHIFT,      KC_Q,           KC_W,           KC_E,           KC_R,               KC_T,                   MAC_NT,
+        KC_ENT,         KC_A,           KC_S,           KC_D,           KC_F,               KC_G,
+        KC_TAB,         KC_GRAVE,       KC_Z,           KC_X,           KC_C,               KC_V,                   MAC_PT,
                                         KC_ESC,         _______,        _______,            KC_LALT,                LT(FN, KC_SPACE),
 
                                                                                                    KC_ESC,                  KC_SPACE,
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // right hand
         _______,            KC_6,           KC_7,               KC_8,           KC_9,           KC_0,           KC_BSLASH,
-        MAC_CHL,            KC_J,           KC_L,               KC_U,           KC_Y,           KC_SCOLON,      KC_BSPACE,
-                            KC_M,           KC_N,               KC_E,           KC_I,           KC_O,           KC_QUOTE,
-        MAC_ALF,            KC_K,           KC_H,               KC_COMMA,       KC_DOT,         KC_SLASH,       KC_MINUS,
+        MAC_CHL,            KC_Y,           KC_U,               KC_I,           KC_O,           KC_P,           KC_BSPACE,
+                            KC_H,           KC_J,               KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+        MAC_ALF,            KC_N,           KC_M,               KC_COMMA,       KC_DOT,         KC_SLASH,       KC_MINUS,
                                             MO(PROG),           MO(LOCAL),      LCTL(KC_SLCK),  KC_SLCK,        KC_PAUS,
 
                                                                                                     TG(QWER),               TG(BASE_WIN),
